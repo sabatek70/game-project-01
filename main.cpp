748758082f80217124feae5d45f6a5b90a91963e
sabatek70 <137089474+sabatek70@users.noreply.github.com>
2025-03-29 03:21:38 +0100

    Progress?
    Added some things and went with the idea I had in the beginning to just pack everything into the classes, draw yourself
    onto the screen lol

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 A simple 2d pixel top-down shoot'em up with a very limited color pallete
 
 CURRENTLY WORKING ON: 
-    Figuring out a way to make the systems not to be too over-engineered, just work,
-    but still be open enough for more functions lol I should probably just make it work
-    and think later... But thinking it through now will save me a lot of reworking later too
-    [...] I better figure it out!
-
+    - Make Backgrounds for topbar, sidebar and levelcounter areas * implement them
+    - Make Textures for the Player and Enemies
+    
+    
 Planned general:
     - AN EPIC MENU with a HIGHSCORE!!!!
     sidebar:
@@ -25,8 +24,8 @@ Planned general:
     - different ships? with different attacks?
     
     TODO artistical direction edition:
-    - Make Backgrounds for topbar, sidebar and levelcounter areas * implement them
-    - Make Textures for the Player and Enemies
+    
+    
     - Make a -simple- fancy font
     - Design a title for the MENU
     - Come up with a better game name than whats coded in right now...
@@ -42,12 +41,9 @@ Planned general:
 #define topbar_height 64
 #define sidebar_width 96
 
-void Initialization();
-void Deinitialization();
 void Calculation();
 void Drawing();
-void GameLoop();
-void LoadLevel();
+void LoadTextures();
 
 const struct Window {
     const char* title = "Galaxory";
@@ -55,68 +51,176 @@ const struct Window {
     int height = gamearea_square + topbar_height;
 } window;
 
-class SquareArea {
-    public:
+Image IMAGES[64];
+Texture2D TEXTURES[64];
+enum TextureEnum
+{
+    NullTexture = 0,
+    PlayerTexture = 10,
+    EnemyEyeTexture = 20,
+    Enemy2Texture = 21,
+    Enemy3Texture = 22,
+    TopbarTexture = 30,
+    LevelCounterTexture = 31,
+    SidebarTexture = 40,
+    GameArea1Texture = 50
+};
+
+class StaticSquareArea {
+    protected:
         Vector2 position;
         Vector2 size;
+    public:
+        Vector2 getPosition() {
+            return this->position;
+        }
+        Vector2 getSize() {
+            return this->size;
+        }
 };
-class TopBar : public SquareArea
+class hasTexture {
+    protected:
+        enum TextureEnum textureID;
+    public:
+        void setTextureID(enum TextureEnum ID) {
+            this->textureID = ID;
+        }
+        enum TextureEnum getTextureID() {
+            return this->textureID;
+        }
+};
+class TopBar : public StaticSquareArea, public hasTexture
+{
+    
+    public:
+        TopBar()
+        {
+            position = {0, 0};
+            size = {(float)window.width - sidebar_width, topbar_height};
+        }
+        void setScore(int number) {
+            this->score = number;
+        }
+        int getScore() {
+            return this->score;
+        }
+    private:
+        int score = 0;
+};
+class SideBar : public StaticSquareArea, public hasTexture
 {
     public:
-    TopBar()
-    {
-        position = {0, 0};
-        size = {(float)window.width - sidebar_width, topbar_height};
-    }
-    void DrawBackground()
-    {
-        DrawRectangleV(this->position, this->size, GREEN);
-    }
+        SideBar()
+        {
+            position = {gamearea_square, topbar_height};
+            size = {sidebar_width, gamearea_square};
+            setTextureID(SidebarTexture);
+        }
+        void setLives(int number) {
+            this->lives = number;
+        }
+        void setProgress(int number) {
+            this->progress = number;
+        }
+        void draw()
+        {
+            DrawTextureV(TEXTURES[this->textureID], this->position, WHITE);
+            DrawTextureV(TEXTURES[this->textureID+1], this->position, WHITE);
+            DrawTextureV(TEXTURES[this->textureID+2], this->position, WHITE);
+        }
+    private:
+        int lives = 3;
+        int progress = 0;
 };
-class SideBar : public SquareArea
+class LevelCounter : public StaticSquareArea, public hasTexture
 {
     public:
-    SideBar()
-    {
-        position = {gamearea_square, topbar_height};
-        size = {sidebar_width, gamearea_square};
-    }
-    void DrawBackground()
-    {
-        DrawRectangleV(this->position, this->size, BLUE);
-    }
+        LevelCounter()
+        {
+            position = {gamearea_square, 0};
+            size = {sidebar_width, topbar_height};
+        }
+        void setLevel(int number) {
+            this->level = number;
+        }
+    private:
+        int level = 1;
 };
-class GameArea : public SquareArea
+class GameArea : public StaticSquareArea, public hasTexture
 {
     public:
-    GameArea()
-    {
-        position = {0, topbar_height};
-        size = {gamearea_square, gamearea_square};
-    }
-    void DrawBackground()
-    {
-        DrawRectangleV(this->position, this->size, RED);
-    }
+        GameArea()
+        {
+            position = {0, topbar_height};
+            size = {gamearea_square, gamearea_square};
+            setTextureID(GameArea1Texture);
+        }
 };
-class LevelCounter : public SquareArea
-{
-    public:
-    LevelCounter()
-    {
-        position = {gamearea_square, 0};
-        size = {sidebar_width, topbar_height};
-    }
-    void DrawBackground()
-    {
-        DrawRectangleV(this->position, this->size, YELLOW);
-    }
-};
+
 class Entity
 {
     public:
         Vector2 position;
-        Rectangle Texture;
+        Vector2 size;
+        Vector2 velocity;
+        void move(){
+            this->position.x += this->velocity.x;
+            this->position.y += this->velocity.y;
+        }
+    private:
+        int life = 0;
+};
+class canShoot
+{
+    protected:
+        int damage = 0;
+    public:
+        void shoot();
+};
+enum bulletType {
+    PLAYER,
+    ENEMY
+};
+class Bullet : public Entity
+{
+    
+    private:
+        Vector2 velocity;
+    public:
+    
+        Bullet(enum bulletType type)
+        {
+            switch(type) {
+                case PLAYER:
+                    velocity = {0, -1};
+                    break;
+                case ENEMY:
+                    velocity = {0, 1};
+                    break;
+            }
+            
+        }
+};
+
+class Player: public Entity, public hasTexture, public canShoot
+{
+    public:
+        Player()
+        {
+            size = {64, 96};
+            position = {(float)gamearea_square/2 - this->size.x/2, (float)window.height - this->size.y*2};
+            setTextureID(PlayerTexture);
+        }
+        void shoot() {
+            new Bullet(PLAYER);
+        }
+        void draw() {
+            DrawTextureV(TEXTURES[this->textureID], this->position, RED);
+        }
+};
+class Enemy: public Entity, public hasTexture
+{
+    
     
 };
 
@@ -124,47 +228,67 @@ TopBar topbar;
 SideBar sidebar;
 GameArea gamearea;
 LevelCounter levelcounter;
+Player player;
 
 int main( void )
 {
-    Initialization();
-    GameLoop();
-    Deinitialization();
+    InitWindow(window.width, window.height, window.title);
+    SetTargetFPS(60);
     
-    return 0;
-}
-void GameLoop()
-{
+    LoadTextures();
     while (!WindowShouldClose())
     {
         Calculation();
         Drawing();
     }
-}
-void Initialization()
-{
-    InitWindow(window.width, window.height, window.title);
-    SetTargetFPS(60);
     
-}
-void Deinitialization()
-{
     CloseWindow();
+    return 0;
 }
+void LoadTextures()
+{
+        IMAGES[0] = LoadImage("res/null.png");
+        IMAGES[10] = LoadImage("res/player.png");
+        IMAGES[20] = LoadImage("res/enemyEye.gif");
+        IMAGES[40] = LoadImage("res/sidebar_background.png");
+        IMAGES[41] = LoadImage("res/sidebar_progress_background.png");
+        IMAGES[42] = LoadImage("res/sidebar_progress_background_display.png");
+        IMAGES[50] = LoadImage("res/starsold.png");
+        for (int i = 0; i < 64; i++)
+        {
+            if(IsImageValid(IMAGES[i]))
+            TEXTURES[i] = LoadTextureFromImage(IMAGES[i]);
+        }
+}
+
 void Calculation()
 {
+    if (IsKeyDown(KEY_A)){
+        player.velocity.x += -1.0;
+    }
+    if (IsKeyDown(KEY_D)){
+        player.velocity.x += 1.0;
+    }
+    
+    if (player.velocity.x > 0) {
+        player.velocity.x -= 0.3;
+    }
+    if (player.velocity.x < 0) {
+        player.velocity.x += 0.3;
+    }
+    if (player.velocity.x < 0.21 && player.velocity.x > -0.21) {
+        player.velocity.x = 0;
+    }
+    player.move();
     return;
 }
 void Drawing()
 {
     BeginDrawing();
-    
     ClearBackground(BLACK);
     
-    topbar.DrawBackground();
-    sidebar.DrawBackground();
-    gamearea.DrawBackground();
-    levelcounter.DrawBackground();
+    player.draw();
+    sidebar.draw();
     
     EndDrawing();
 }
